34163d7c2cc2f2c557281a65184daea9f71d8f37
machadovilaca <machadovilaca@gmail.com>
2019-02-28 20:01:08 +0000

    New sorting algorithm

--- src/main/java/PhotoMap.java
+++ src/main/java/PhotoMap.java
@@ -1,32 +1,67 @@
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
 
 public class PhotoMap {
-    private Map<Integer, PhotoRelationships> photoRelationshipsMap;
-    private Map<Integer, Photo> photos;
+    private Map<Integer, Slide> photos;
 
-    public PhotoMap(){
-        this.photoRelationshipsMap = new HashMap<>();
+    PhotoMap(){
         this.photos = new HashMap<>();
     }
 
-    public void add(Photo photo){
-        photoRelationshipsMap.forEach((k,v) ->
-            v.addToList(photo)
-        );
+    void add(Slide slide){
+        this.photos.put(slide.getId(), slide);
+    }
 
-        PhotoRelationships list = new PhotoRelationships(photo);
-        this.photos.put(photo.getId(), photo);
-        //this.photoRelationshipsMap.put(photo.getId(),list);
+    private int min(Slide first, Slide newPhoto) {
+        int min, e, comuns = 0, d;
+
+        for(String tag : first.getTags()) {
+            if(newPhoto.getTags().contains(tag))
+                comuns++;
+        }
+
+        e = first.getTags().size() - comuns;
+        d = newPhoto.getTags().size() - comuns;
+
+        min = e;
+        if (comuns < min) min = comuns;
+        if(d < min) min = d;
+
+        return min;
+    }
+
+    private boolean noInicio(Slide first, Slide last, Slide newPhoto) {
+        return min(first, newPhoto) > min(last, newPhoto);
     }
 
     public void createSlideshow(){
-        //todo implement
+        System.out.println(photos.size());
+        LinkedList<String> a = new LinkedList<>();
+        Slide first = null;
+        Slide last = null;
+
+        for(Slide v : photos.values()) {
+            if(first == null) {
+                first = last = v;
+                a.add(v.getSlidePhotos());
+            } else {
+                if(noInicio(first, last, v)) {
+                    first = v;
+                    a.addFirst(v.getSlidePhotos());
+                } else {
+                    last = v;
+                    a.addLast(v.getSlidePhotos());
+                }
+            }
+        }
+
+        a.forEach(System.out::println);
     }
 
-    public void paraString(){
+    void paraString(){
         int i = 0;
-        for(Photo p: this.photos.values()){
+        for(Slide p: this.photos.values()){
             System.out.println("Foto nr: " + p.getId());
             for(String s: p.getTags()){
                 System.out.println("Tag " + i + ": " + s);
